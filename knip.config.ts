3d504d7c617a3200e740ad305b16f6caf22cc9e0
Sam Elie <samradelie@gmail.com>
2025-10-24 09:03:07 -0700

    big progress parks

--- knip.config.ts
+++
@@ -1,6 +0,0 @@
-import { defineKnipConfig } from "@adddog/config-defaults/knip.config.js";
-
-export default defineKnipConfig({
-    entry: ["src/index.ts", "src/**/__tests__/**/*.test.ts"],
-    project: ["src/**/*.ts"]
-});
