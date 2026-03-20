9443b164c012c82f7067dfdcd242064594b4a593
Sam Elie <samradelie@gmail.com>
2026-03-19 20:00:27 -0700

    paradocs wipxy

--- knip.config.ts
+++ knip.config.ts
@@ -7,6 +7,7 @@ export default defineKnipConfig({
     project: ["src/**/*.ts"],
 }, {
     ignoreDependencies: [
+        "@adddog/build-configs",
         "@adddog/monorepo-consistency",
         "@vitest/coverage-v8",
     ],
